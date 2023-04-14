c0ca6cb108e8b26e21cb4d24f157e36e5441f93c
perotedev <rodripema@hotmail.com>
2023-04-14 19:14:49 -0400

    [FEAT] - Implements the test

--- src/app/modules/sound-game/know-rule/know-rule.component.ts
+++ src/app/modules/sound-game/know-rule/know-rule.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HtmlUtils } from 'src/app/shared/utils/HtmlUtils';
+import { QuestionDescription, questions } from './questions';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-know-rule',
@@ -6,5 +9,97 @@ import { Component } from '@angular/core';
   styleUrls: ['./know-rule.component.scss']
 })
 export class KnowRuleComponent {
+  public isMobile: boolean = HtmlUtils.isMobileDevice();
+  public textoPergunta: string = "";
+  public typeQuestion: number = 0;
+  public subType: number = 0;
+  public qtdAnswers: number = 1;
+  private itens: Array<QuestionDescription> = questions;
+  private itemsVisitados: Array<number> = [];
+  private itemAtual: number;
 
+  constructor(){
+    this.itemAtual = Math.floor(Math.random() * this.itens.length);
+    this.selectQuestion();
+  }
+
+  private generateItem(){
+    while (this.itemsVisitados.includes(this.itemAtual)){
+      this.itemAtual = Math.floor(Math.random() * this.itens.length);
+    }
+    this.itemsVisitados.push(this.itemAtual);
+  }
+
+  private selectQuestion(): void {
+    this.generateItem();
+
+    if (this.itens[this.itemAtual].toca){
+      this.generateType0();
+    } else {
+      this.generateType1();
+    }
+  }
+
+  private generateType0(): void {
+    this.subType = Math.floor(Math.random() * 2);
+    this.typeQuestion = 0;
+    if (this.subType === 0) {
+      this.textoPergunta = `"${this.itens[this.itemAtual].item}" toca quantas vezes?`
+    } else {
+      this.generateType1();
+    }
+  }
+
+  private generateType1(): void {
+    this.typeQuestion = 1;
+    this.textoPergunta = `"${this.itens[this.itemAtual].item}" toca?`
+  }
+
+  public tryAnswer0(value: number): void {
+    if (value === this.itens[this.itemAtual].toques){
+      this.corretAnswer();
+    } else {
+      this.wrongAnswer();
+    }
+  }
+  
+  public tryAnswer1(value: boolean): void {
+    if(value === this.itens[this.itemAtual].toca){
+      this.corretAnswer();
+    } else {
+      this.wrongAnswer();
+    }
+  }
+
+  private corretAnswer(){
+    Swal.fire({
+      title: "Resposta Correta",
+      icon: "success",
+      confirmButtonText: "OK",
+    });
+
+    this.selectQuestion();
+
+    if (this.qtdAnswers < 21){
+      this.addCount();
+    }
+  }
+
+  private wrongAnswer(){
+    Swal.fire({
+      title: "Resposta Errada",
+      icon: "error",
+      confirmButtonText: "OK",
+    });
+
+    this.back();
+  }
+
+  private addCount(): void {
+    this.qtdAnswers ++;
+  }
+
+  public back(): void {
+    HtmlUtils.previusPage();
+  }
 }
---
+++ src/app/modules/sound-game/know-rule/questions.ts
@@ -0,0 +1,393 @@
+export interface QuestionDescription {
+    item: string;
+    toca: boolean;
+    toques: number;
+}
+
+export const questions: Array<QuestionDescription> = [
+    {
+        item: "Carro",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Balão",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Papelão",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Parabrisa",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Laranja",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Papel",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Brinquedo",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Som",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Computador",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Grama",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Belém",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Caneta",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Violino",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Piano",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Tuba",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Flauta",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Saxofone",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Banheiro",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Pombo",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Cobra",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Guardanapo",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Bateria",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Argentina",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Bolívia",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Samaritano",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Bárbara",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Bombeiro",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Almanaque",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Geladeira",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Alicate",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Clarinete",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Corneta",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Buzina",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Lâmpada",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Batalhão",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Memorial",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Alberto",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Basquete",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Biblioteca",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Futebol",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Purpurina",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Navio",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Mingau",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Danilo",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Janela",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Pupunha",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Televisão",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Celular",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Aplicativo",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Mesa",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Paralelogramo",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Moto",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Borboleta",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Combo",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Cinema",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Apresentador",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Aparelho",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Cooperação",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Colina",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Doutor",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Camareira",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Eleição",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Bicicleta",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Bambolê",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Bimotor",
+        toca: true,
+        toques: 2
+    },
+    {
+        item: "Tomada",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Gelo",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Álbum",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Fim",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Réquiem",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Samba",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Médium",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Item",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Desdém",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Dom",
+        toca: false,
+        toques: 0
+    },
+    {
+        item: "Sistema",
+        toca: true,
+        toques: 1
+    },
+    {
+        item: "Espectro",
+        toca: true,
+        toques: 1
+    }
+]

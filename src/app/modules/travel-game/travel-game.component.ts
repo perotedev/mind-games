211a0e0dea7af19ff24730e75ce82707d9f8d62b
perotedev <rodripema@hotmail.com>
2023-04-16 11:14:51 -0400

    [FIX] - Fix travel-game black input

--- src/app/modules/travel-game/travel-game.component.ts
+++ src/app/modules/travel-game/travel-game.component.ts
@@ -18,6 +18,7 @@ export class TravelGameComponent {
   public dicas: Array<string> = ["Venezuela", "Ottawa", "Luxemburgo", "Tailândia", "Argentina"];
   public transportes: Array<string> = ["Carro", "Moto", "Barco", "Avião", "Bicicleta", "Trem"];
   
+  private indexTransporte: number = 0;
   private ruleIndex: number = 0;
   private gameRule: Array<string> = ['v', 'o', 'l', 't', 'a'];
   
@@ -26,7 +27,7 @@ export class TravelGameComponent {
       const entradaOriginal: string = value;
       const palavra: string = entradaOriginal.toLowerCase();
       const letra: string = palavra.normalize("NFD").replace(/\p{Diacritic}/gu, "").slice(0,1);
-      let mensagem = `Você não chegou em ${entradaOriginal}.`;
+      let mensagem = `Você não chegou em ${entradaOriginal}`;
 
       this.palpites.unshift({
         input: entradaOriginal,
@@ -38,8 +39,9 @@ export class TravelGameComponent {
       }
 
       if (this.gameRule[this.ruleIndex] === letra){
-        mensagem = `Você chegou em ${entradaOriginal}.`;
+        mensagem = `Você chegou em ${entradaOriginal}`;
         this.cidadeAtual = `Você está em ${entradaOriginal}`
+        this.indexTransporte = Math.floor(Math.random() * this.transportes.length);
         this.showPopupResultado(mensagem, true);
         this.ruleIndex++;
       } else {
@@ -49,9 +51,13 @@ export class TravelGameComponent {
       if (this.ruleIndex > 4){
         this.ruleIndex = 0;
       }
+    } else if (value.length < 4) {
+      this.showAlert(
+        "Digite um local de destino com pelo menos 4 letras!"
+      )
     } else {
       this.showAlert(
-        "Selecione um MEIO DE TRANSPORTE!"
+        "Selecione um meio de transporte!"
       )
     }
   }
@@ -67,8 +73,12 @@ export class TravelGameComponent {
     }
     
     if (value){
-      this.showAlert(`Seu próximo destino pode ser ${this.dicas[dicaIndex]}`)
+      this.showAlert(`Seu próximo destino pode ser ${this.dicas[dicaIndex]} indo de ${this.getTipVeicle()}`)
     }
+  }
+
+  private getTipVeicle(): string {
+    return this.transportes[this.indexTransporte].toLowerCase();
   }
 
   private showPopupResultado(mensagem: string, chegou:boolean): void {
